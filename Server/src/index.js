4d0761acc9a5648e71a0f2441d55549d83831fd1
aguscabralr <acabralrobledo@gmail.com>
2023-05-17 01:44:40 -0300

    Server 16/05

---
+++ Server/src/index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const characters = require("./utils/data")
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.url.includes('/rickandmorty/character')) {
+        let id = req.url.split('/').at(-1);
+        let character = characters.filter(char => char.id === Number(id));
+        res.writeHead(200, { 'Content-Type': 'application/json' }).end(JSON.stringify(character));
+    };
+}).listen(3001, 'localhost');
